22f3271910a5be34554e4092567f472390e4bc91
IgorNascimento__ <137299327+Demiurge0@users.noreply.github.com>
2024-03-14 15:17:19 -0300

    Add files via upload

---
+++ Cliente.py
@@ -0,0 +1,13 @@
+class Cliente():
+    def __init__(self, n, fone):
+
+        self.nome = n
+        self.telefone = fone
+
+        # Método get
+        def get_nome(self):
+            return self.nome
+
+        # Método set
+        def set_nome(self, nome):
+            self.nome = nome
